050b3baa2ccbc69e801b60dbe117a29e174716e4
kent.chen <kent.chen@hwacom.com>
2021-11-25 16:44:46 +0800

    #18 edit constuctor of Sheet

--- WarframeShawzin/core/classes/Sheet.py
+++ WarframeShawzin/core/classes/Sheet.py
@@ -3,20 +3,34 @@ from ..Attributes import SCALE_DICT
 from .Note import Note
 
 class Sheet:
-    def __init__(self, sheet_str):
-        self._encoded_sheet = sheet_str
-        # first digit is scale
-        self._scale = int(self._encoded_sheet[0])
-        self._sheet_str = self._encoded_sheet[1:]
-        # check if scale is available
-        assert self._scale <= 9 and self._scale >= 1, \
-            'scale should be an integer between 1 to 9'
-        # check if sheet_str is multiple of 3
-        assert len(self._sheet_str)%3 == 0, 'sheet_str should be multiple of 3'
+    def __init__(self, *args):
+        if len(args) == 1:
+            sheet_obj = args[0]
+            if isinstance(sheet_obj, str):
+                self._encoded_sheet = sheet_obj
+                # first digit is scale
+                self._scale = int(self._encoded_sheet[0])
+                self._sheet_str = self._encoded_sheet[1:]
+                # check if scale is available
+                assert self._scale <= 9 and self._scale >= 1, \
+                    'scale should be an integer between 1 to 9'
+                # check if sheet_str is multiple of 3
+                assert len(self._sheet_str)%3 == 0, 'sheet_str should be multiple of 3'
 
-        # list of notes
-        self._notes_list = list()
-        self._parsing()
+                # list of notes
+                self._notes_list = list()
+                self._parsing()
+            else:
+                raise Exception("The parameter type {} used for constructor of Sheet is unacceptable.".format(type(sheet_obj)))
+
+        elif len(args) == 2:
+            if isinstance(args[1], list):
+                self._scale = args[0]
+                self._notes_list = args[1]
+            else:
+                raise Exception("The parameter type {} used for constructor of Sheet is unacceptable.".format(type(sheet_obj)))
+        else:
+            raise Exception("The number of parameters {} used for constructor of Sheet is unacceptable.".format(len(args)))
 
     def __str__(self):
         output_str = 'scale: {}, notes: [\n'.format(SCALE_DICT[self._scale])
@@ -46,7 +60,7 @@ class Sheet:
 
     # clone a new sheet obj
     def clone(self):
-        return Sheet(self._encoded_sheet)
+        return Sheet(self._scale, self._notes_list)
 
     # getter and setter
     def get_notes_list(self):
